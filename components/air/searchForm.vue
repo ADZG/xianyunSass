dac33b243a4df72d9af74d045772867c63e72568
“zg <1589683282@qq.com>
2019-07-01 21:16:01 +0800

    修正了分页的错误

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -59,6 +59,7 @@
   </div>
 </template>
 <script>
+import { promised } from "q";
 export default {
   data() {
     return {
@@ -84,113 +85,90 @@ export default {
   methods: {
     handleSearch(index) {
       if (index === 1) {
-        this.$confirm("目前暂不支持往返，请使用单程选票！", "提示", {
-          confirmButtonText: "确定",
-          showCancelButton: false,
-          type: "warning"
-        });
+        this.$alert("该模块正在研发中", "哈咯");
       }
     },
-    // 搜索出发点
-    // 出发城市输入框获得焦点时触发
-    // value 是选中的值，cb是回调函数，接收要展示的列表
-    SearchStart(value, cb) {
-      this.querySearchAsync(value).then(res => {
-        if (res.length > 0) {
-          // 如果不在下拉列表选择，则默认选中第一项
-          this.form.departCity = res[0].value;
-          this.form.departCode = res[0].sort;
-        }
-        cb(res);
-      });
+    async SearchStart(value, cb) {
+      let arr = await this.asyncQuery(value);
+      if (arr.length > 0) {
+        this.form.departCity = arr[0].value;
+        this.form.departCode = arr[0].sort;
+      }
+      cb(arr);
     },
-    // 搜索到达点
-    SearchArrive(value, cb) {
-      this.querySearchAsync(value).then(res => {
-        if (res.length > 0) {
-            // 如果用户有输入，但是选择，则默认选中第一项
-          // 如果不在下拉列表选择，则默认选中第一项
-          this.form.destCity = res[0].value;
-          this.form.destCode = res[0].sort;
-        }
-        cb(res);
-      });
+    async SearchArrive(value, cb) {
+      let arr = await this.asyncQuery(value);
+      if (arr.length > 0) {
+        // 当输入框有输入的时候，先默认选中第一个
+        this.form.destCity = arr[0].value;
+        this.form.destCode = arr[0].sort;
+      }
+      cb(arr);
     },
-    // 选择出发点触发的事件
     handleSelectStart(v) {
       this.form.departCity = v.value;
       this.form.departCode = v.sort;
     },
-    // 选择抵达点触发的事件
     handleSelectArrive(v) {
       this.form.destCity = v.value;
       this.form.destCode = v.sort;
     },
-    handleStartTime(time) {
-      this.form.departDate = time;
+    handleStartTime(v) {
+      this.form.departDate = v;
     },
-    // 输入信息交换
-    change() {
-      let { departCity, departCod, destCity, destCode } = this.form;
-      this.form.departCity = destCity;
-      this.form.departCode = destCode;
-      this.form.destCity = departCity;
-      this.form.destCode = departCod;
-    },
-    // 搜索航班
     submitForm() {
-      //   先验证表单内是否有数据
       const rules = {
-        depart: { value: this.form.departCity, message: "请选择出发城市" },
-        dest: { value: this.form.destCity, message: "请选择到达城市" },
-        departDate: { value: this.form.departDate, message: "请选择到达日期" }
+        depart: {
+          value: this.form.departCity,
+          message: "请输入出发城市名"
+        },
+        dest: {
+          value: this.form.destCity,
+          message: "请输入抵达城市名"
+        }
       };
       let valid = true;
       Object.keys(rules).forEach(v => {
-        if (!v) return;
-        const item = rules[v];
-        if (!item.value) {
-          valid = false;
-          this.$confirm(item.message, "提示", {
-            confirmButtonText: "确定",
-            showCancelButton: false,
-            type: "warning"
-          });
-        }
-        // 如果valid为false 则不用继续向下执行
         if (!valid) return;
-        this.$router.push({
-          path: "/air/flights",
-          query: this.form
+        Object.keys(rules).forEach(v => {
+          if (!valid) return;
+          const item = rules[v];
+          if (!item.value) {
+            valid = false;
+            this.$confirm(item.message, "提示", {
+              confirmButtonText: "确定",
+              showCancelButton: false,
+              type: "warning"
+            });
+          }
         });
-        this.$store.commit("air/getSearchDate",this.form)
+      });
+      if (!valid) return; //如果验证不通过，则不继续执行
+      this.$router.push({
+        path: "air/flights",
+        query: this.form
       });
     },
-    // 查询城市的方法
-    querySearchAsync(queryString) {
+    asyncQuery(queryString) {
       return new Promise((resolve, reject) => {
-        // 如果关键字是空，则直接返回
         if (!queryString) {
           return resolve([]);
         }
-        this.$axios({
+        return this.$axios({
           url: `/airs/city`,
           params: {
             name: queryString
           }
         }).then(res => {
           const { data } = res.data;
-          // 下拉提示列表必须要有value字段
           const arr = data.map(v => {
-            return {
-              ...v,
-              value: v.name.replace("市", "")
-            };
+            return { ...v, value: v.name.replace("市", "") };
           });
           resolve(arr);
         });
       });
-    }
+    },
+    change() {}
   }
 };
 </script>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -6,7 +6,7 @@
       <div class="flights-content">
         <!-- 过滤条件 -->
         <div>
-          <FlightsFilters :data="flightsData" @setDataList="setDataList"></FlightsFilters>
+          <FlightsFilters :data="cacheFlightsData" @setDataList="setDataList"></FlightsFilters>
         </div>
 
         <!-- 航班头部布局 -->
@@ -19,6 +19,11 @@
           <!-- 这里是一块数据，但是遍历出了多个 -->
           <FlightsItem v-for="(item, index) in dataList" :key="index" :data="item"></FlightsItem>
           <el-row type="flex" justify="center" style="margin-top:10px;">
+            <!-- size-change：切换条数时候触发 -->
+            <!-- current-change：选择页数时候触发 -->
+            <!-- current-page: 当前页数 -->
+            <!-- page-size：当前条数 -->
+            <!-- total：总条数 -->
             <el-pagination
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
@@ -26,7 +31,7 @@
               :page-sizes="[5, 10, 15, 20]"
               :page-size="pageSize"
               layout="total, sizes, prev, pager, next, jumper"
-              :total="flightsData.total+0"
+              :total="total"
               :background="true"
             ></el-pagination>
           </el-row>
@@ -48,15 +53,21 @@ export default {
     return {
       pageIndex: 1, //当前的页码
       pageSize: 5, //每页显示的条数
+      total:0,
       flightsData: {
         // 航班总数据
         //由于数据请求是异步的，需要先给子组件一个空数据，不至于报错，
         flights: [],
         info: {},
         options: {},
-        total:""
       }, //航班的总数据  这里总数据其他的组件会用得到，比如顶部搜索的历史等等
-      dataList: [] //航班列表数据
+      dataList: [], //航班列表数据
+      cacheFlightsData: {
+        // 缓存一份数据，只会修改一次
+        flights: [],
+        info: {},
+        options: {},
+      }
     };
   },
   components: {
@@ -67,26 +78,26 @@ export default {
   methods: {
     // 分割数据数组，形成新的筛选过的数组，子组件去显示
     setDataList(arr) {
-         if(arr){
-            //  如果筛选后有数据传输进来
-            // 设置当前当前的页码
-            // 将当前显示的数据，替换成筛选过的arr数据
-            // 将数据条数，替换成筛选过后的数组的长度
-             this.pageIndex=1
-             this.flightsData.flights=arr
-             this.flightsData.total=arr.length
-         }
-        // 这里在子组件发射了一个事件，并且事件里有数组数据，监听自定义事件，只要子组件上该事件一触发，则这个事件也触发
-        // 只是名称一样
+      if (arr) {
+        //  如果筛选后有数据传输进来
+        // 设置当前当前的页码
+        // 将当前显示的数据，替换成筛选过的arr数据
+        // 将数据条数，替换成筛选过后的数组的长度
+        this.flightsData.flights=arr
+        this.pageIndex = 1;
+        this.total= arr.length;
+      }
+      // 这里在子组件发射了一个事件，并且事件里有数组数据，监听自定义事件，只要子组件上该事件一触发，则这个事件也触发
+      // 只是名称一样
       //   搜索之后，服务器将所有的数据全部返回来，要自己进行筛选
-      const start = (this.pageIndex - 1) * this.pageSize; //分页是从1开始，而数组分隔是从索引0开始, 得到分割开始的索引
-      const end = start + this.pageSize; //得到分割结束的索引
-      this.dataList = this.flightsData.flights.splice(start, end); //找到当前机票数据列表,切割后返回一个新的数组
+      let start = (this.pageIndex - 1) * this.pageSize; //分页是从1开始，而数组分隔是从索引0开始, 得到分割开始的索引
+      let end =this.pageIndex* this.pageSize; //得到分割结束的索引
+      this.dataList = this.flightsData.flights.slice(start, end); //找到当前机票数据列表,切割后返回一个新的数组
     },
     //    切换条数触发
     handleSizeChange(v) {
       this.pageSize = v;
-      this.pageIndex = 1;
+      // this.pageIndex = 1;
       this.setDataList();
       //   在当前页码或者每页显示的条数改变的时候，都重新切割一份新的数组，在页面显示
     },
@@ -94,8 +105,7 @@ export default {
     handleCurrentChange(v) {
       this.pageIndex = v;
       this.setDataList();
-    },
-
+    }
   },
   mounted() {
     this.$axios({
@@ -104,8 +114,10 @@ export default {
     }).then(res => {
       this.flightsData = res.data;
       this.dataList = this.flightsData.flights;
-      this.setDataList();
-    //   console.log(res.data);
+      // this.setDataList();
+      this.cacheFlightsData={...res.data}
+      //   console.log(res.data);
+      this.total=this.flightsData.total
     });
   }
 };
